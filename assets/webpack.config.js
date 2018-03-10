f21dae646806b3275c9bbf838752adc68ab9207a
scorsi <sylvain.corsini@gmail.com>
2018-03-10 01:00:34 +0100

    Refont assets and (pre)add Elm for front

---
+++ assets/webpack.config.js
@@ -0,0 +1,45 @@
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+
+  entry: './js/app',
+  output: {
+    path: path.resolve(__dirname, '../priv/static'),
+    filename: 'js/app.js'
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: [/node_modules/, /priv\/static/],
+        use: {
+          loader: 'babel-loader',
+          //options: {
+          //  presets: ['env']
+          //}
+        }
+      },
+      {
+        test: /\.css$/,
+        use: ExtractTextPlugin.extract({
+          fallback: "style-loader",
+          use: [ "css-loader" ]
+        })
+      },
+      {
+        test: /\.elm$/,
+        exclude: [/elm-stuff/, /node_modules/, /tests/],
+        use: {
+          loader: 'elm-webpack-loader',
+          options: { debug: true }
+        }
+      }
+    ],
+    noParse: [/\.elm$/]
+  },
+
+  plugins: [ new ExtractTextPlugin("css/app.css") ]
+
+};
