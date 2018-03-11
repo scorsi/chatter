f2492d2342c60a4d54338e3374e4892212e5a826
scorsi <sylvain.corsini@gmail.com>
2018-03-11 23:18:49 +0100

    Add authorization header to graphQL requests

--- web/src/main.js
+++ web/src/main.js
@@ -3,16 +3,28 @@ import App from '@/App'
 import router from '@/router'
 import Vuetify from 'vuetify'
 import { ApolloClient } from 'apollo-client'
-import { HttpLink } from 'apollo-link-http'
+import { createHttpLink } from 'apollo-link-http'
+import { setContext } from 'apollo-link-context'
 import { InMemoryCache } from 'apollo-cache-inmemory'
 import VueApollo from 'vue-apollo'
 import gql from 'graphql-tag'
 import '@/stylus/main.styl'
 
+const httpLink = createHttpLink({
+  uri: 'http://localhost:4000/api'
+})
+
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem('token')
+  return {
+    headers: {
+      authorization: token ? `Bearer ${token}` : ''
+    }
+  }
+})
+
 const apolloClient = new ApolloClient({
-  link: new HttpLink({
-    uri: 'http://localhost:4000/api'
-  }),
+  link: authLink.concat(httpLink),
   cache: new InMemoryCache(),
   connectToDevTools: true
 })
